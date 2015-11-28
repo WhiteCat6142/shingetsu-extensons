ad779c24d7b3791fb3b7aba5497b811c04701f5d
WhiteCat6142 <WhiteCat6142@users.noreply.github.com>
2015-11-27 23:16:58 -0800

    Update md.js

--- md.js
+++ md.js
@@ -7,7 +7,7 @@ shingetsu.initialize(function(){
  $("dd").each(function(i,ele){
   var t=$(ele).text();
   if(t.substring(0,"@markdown".length)=="@markdown"){
-   $(ele).html(marked(t.substring("@markdown".length)));
+   $(ele).html(marked(t.substring("@markdown".length).replace(/    :::c/g,"```c++").replace(/    :::/g,"```")));
   }
  });
 });
