ebf2024057bbb6904855ff66fdfbe6ecefa28106
WhiteCat6142 <WhiteCat6142@users.noreply.github.com>
2015-12-04 16:04:39 -0800

    Update md.js

--- md.js
+++ md.js
@@ -5,6 +5,7 @@ marked.setOptions({
 });
 shingetsu.initialize(function(){
  $("dd").each(function(i,ele){
+  if($(ele).children().not("br").length)return;
   var t=$(ele).text();
   if(t.substring(0,"@markdown".length)=="@markdown"){
    $(ele).html(marked(t.substring("@markdown".length)));
